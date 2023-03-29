cca431ae05a8f28ca3f360a45f71b2bc3f75bfae
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2023-03-29 19:05:17 +0600

    dashboard header is on work

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Dashboard = () => {
+  const [activeSidebar, setActiveSidebar] = useState(true);
   return (
-    <div className='flex items-center justify-center min-h-screen text-bold text-primary text-sm'>Dashboard page starts here</div>
-  )
-}
+    <div className="flex">
+      <div
+        className={`${
+          activeSidebar ? "w-2/12" : "w-20"
+        } transition-all duration-75 `}
+      >
+        <ul className="grid gap-1 px-2">
+          <li>btn</li>
+          <li>btn</li>
+          <li>btn</li>
+          <li>btn</li>
+          <li>btn</li>
+          <li>btn</li>
+        </ul>
+      </div>
+      <div
+        className={`${
+          activeSidebar ? "w-10/12" : "w-full"
+        } transition-all duration-75`}
+      >
+        <div className="grid grid-cols-3 content-center p-2 shadow-md">
+          <RxHamburgerMenu
+            className="cursor-pointer text-lg font-semibold"
+            onClick={() => {
+              setActiveSidebar((state) => !state);
+            }}
+          />
+          <div className="">body</div>
+          <div className="">btns</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
